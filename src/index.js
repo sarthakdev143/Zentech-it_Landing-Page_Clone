0184eb2c63dcc5f6f9124cd31aa21dedf5dd5ccd
Sarthak Parulekar <148764141+sarthakdev143@users.noreply.github.com>
2024-06-01 23:06:33 +0530

    "Removed App.css and App.js files, updated package.json, and modified index.js to render Home component instead of App"

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Home from './Components/Home/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-
     <Home />
   </React.StrictMode>
 );
